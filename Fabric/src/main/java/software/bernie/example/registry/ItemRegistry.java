cdf369380ca66932b2b6d25440a9b7e70b0203ca
Colin Vaughn <Colinvaughn01@gmail.com>
2023-05-25 15:33:32 -0400

    aquatic fix

--- Fabric/src/main/java/software/bernie/example/registry/ItemRegistry.java
+++ Fabric/src/main/java/software/bernie/example/registry/ItemRegistry.java
@@ -3,6 +3,7 @@ package software.bernie.example.registry;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.*;
 import software.bernie.example.item.GeckoArmorItem;
@@ -34,9 +35,9 @@ public class ItemRegistry {
     public static final SpawnEggItem FAKE_GLASS_SPAWN_EGG = registerItem("fake_glass_spawn_egg", new SpawnEggItem(EntityRegistry.FAKE_GLASS, 0xDD0000, 0xD8FFF7, new Item.Properties()));
     public static final SpawnEggItem COOL_KID_SPAWN_EGG = registerItem("cool_kid_spawn_egg", new SpawnEggItem(EntityRegistry.COOL_KID, 0x5F2A31, 0x6F363E, new Item.Properties()));
     public static final SpawnEggItem GREMLIN_SPAWN_EGG = registerItem("gremlin_spawn_egg", new SpawnEggItem(EntityRegistry.GREMLIN, 0x505050, 0x606060, new Item.Properties()));
-    
+
     public static final CreativeModeTab ITEM_GROUP = FabricItemGroup
-            .builder(new ResourceLocation(GeckoLib.MOD_ID, "geckolib_examples"))
+            .builder().title(Component.translatable("itemGroup.geckolib.geckolib_examples"))
             .icon(() -> new ItemStack(ItemRegistry.JACK_IN_THE_BOX))
             .displayItems((enabledFeatures, entries) -> {
                 entries.accept(ItemRegistry.JACK_IN_THE_BOX);
@@ -60,6 +61,8 @@ public class ItemRegistry {
                 entries.accept(ItemRegistry.FAKE_GLASS_SPAWN_EGG);
                 entries.accept(ItemRegistry.COOL_KID_SPAWN_EGG);
             }).build();
+
+
 
     public static <I extends Item> I registerItem(String name, I item) {
         return Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(GeckoLib.MOD_ID, name), item);
